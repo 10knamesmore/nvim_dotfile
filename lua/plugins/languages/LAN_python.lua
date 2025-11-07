1b016e68cc8dd2c7dbc2a3c9d3225f760c458eeb
wanger <310779211wym@gmail.com>
2025-11-07 23:11:34 +0800

    optim python lan

--- lua/plugins/languages/LAN_python.lua
+++ lua/plugins/languages/LAN_python.lua
@@ -1,28 +1,14 @@
 return {
-    {
-        "linux-cultist/venv-selector.nvim",
-        dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
-        opts = {
-            -- Your options go here
-            name = ".venv",
-            auto_refresh = false,
-            stay_on_this_version = true,
-        },
-        event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
-        keys = {
-            -- Keymap to open VenvSelector to pick a venv.
-            { "<leader>vs", "<cmd>VenvSelect<cr>" },
-            -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-            { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
-        },
-    },
-    {
-        "folke/which-key.nvim",
-        optional = true,
-        opts = {
-            spec = {
-                { "<leader>v", group = "python venv" },
-            },
-        },
-    },
+    "linux-cultist/venv-selector.nvim",
+    ft = "python",
+    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+    config = function(_, opts)
+        require("venv-selector").setup(opts)
+        require("which-key").add({
+            "<leader>v",
+            group = "python venv",
+        })
+        vim.keymap.set({ "n", "x" }, "<leader>vs", "<cmd>VenvSelect<cr>")
+        vim.keymap.set({ "n", "x" }, "<leader>vc", "<cmd>VenvSelectCached<cr>")
+    end,
 }
