b0940423e2b7124a9530fceb2040e8c1b89f6adb
wanger <310779211wym@gmail.com>
2026-01-03 23:54:30 +0800

    • feat(mini.files): 增强文件面板分屏与外观
    新增 MiniFiles 窗口样式设置与分屏快捷键，默认隐藏点文件
      以提升浏览体验与操作效率

--- lua/plugins/editor/files/mini-files.lua
+++ lua/plugins/editor/files/mini-files.lua
@@ -1,12 +1,34 @@
 -- mini file 文件浏览/管理
 -- <leader>e 打开于当前文件
 --
-local filter_show = function()
+local filter_show = function(fs_entry)
     return true
 end
 
 local filter_hide = function(fs_entry)
     return not vim.startswith(fs_entry.name, ".")
+end
+
+local map_split = function(buf_id, lhs, direction)
+    local rhs = function()
+        -- Make new window and set it as target
+        local cur_target = MiniFiles.get_explorer_state().target_window
+        local new_target = vim.api.nvim_win_call(cur_target, function()
+            vim.cmd(direction .. " split")
+            return vim.api.nvim_get_current_win()
+        end)
+
+        MiniFiles.set_target_window(new_target)
+
+        require("mini.files").go_in()
+        -- This intentionally doesn't act on file under cursor in favor of
+        -- explicit "go in" action (`l` / `L`). To immediately open file,
+        -- add appropriate `MiniFiles.go_in()` call instead of this comment.
+    end
+
+    -- Adding `desc` will result into `show_help` entries
+    local desc = "Split " .. direction
+    vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
 end
 
 return {
@@ -39,11 +61,11 @@ return {
         require("mini.files").setup(opts)
 
         -- 默认显示
-        local show_dotfiles = true
+        local show_dotfiles = false
 
         local toggle_dotfiles = function()
-            local new_filter = show_dotfiles and filter_show or filter_hide
             show_dotfiles = not show_dotfiles
+            local new_filter = show_dotfiles and filter_show or filter_hide
             require("mini.files").refresh({ content = { filter = new_filter } })
         end
 
@@ -59,6 +81,29 @@ return {
             pattern = "MiniFilesActionRename",
             callback = function(event)
                 require("snacks").rename.on_rename_file(event.data.from, event.data.to)
+            end,
+        })
+
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "MiniFilesWindowOpen",
+            callback = function(args)
+                local win_id = args.data.win_id
+
+                -- Customize window-local settings
+                vim.wo[win_id].winblend = 10
+                local config = vim.api.nvim_win_get_config(win_id)
+                config.border, config.title_pos = "rounded", "right"
+                vim.api.nvim_win_set_config(win_id, config)
+            end,
+        })
+
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "MiniFilesBufferCreate",
+            callback = function(args)
+                local buf_id = args.data.buf_id
+                -- Tweak keys to your liking
+                map_split(buf_id, "<C-s>", "belowright horizontal")
+                map_split(buf_id, "<C-v>", "belowright vertical")
             end,
         })
     end,
