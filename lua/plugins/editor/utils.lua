320a115c222ca56a77194ee3a022f6584c51e9bf
wanger <310779211wym@gmail.com>
2026-02-01 02:10:55 +0800

    update

--- lua/plugins/editor/utils.lua
+++ lua/plugins/editor/utils.lua
@@ -3,43 +3,13 @@
 -- plenary - 其他插件使用的库
 
 return {
-  -- persistence - Session 管理
-  {
-    "folke/persistence.nvim",
-    event = "BufReadPre",
-    opts = {},
-    keys = {
-      {
-        "<leader>qs",
-        function()
-          require("persistence").load()
-        end,
-        desc = "Restore Session",
-      },
-      {
-        "<leader>qS",
-        function()
-          require("persistence").select()
-        end,
-        desc = "Select Session",
-      },
-      {
-        "<leader>ql",
-        function()
-          require("persistence").load({ last = true })
-        end,
-        desc = "Restore Last Session",
-      },
-      {
-        "<leader>qd",
-        function()
-          require("persistence").stop()
-        end,
-        desc = "Don't Save Current Session",
-      },
+    -- persistence - Session 管理
+    {
+        "folke/persistence.nvim",
+        event = "BufReadPre",
+        opts = {},
     },
-  },
 
-  -- plenary - 其他插件使用的库
-  { "nvim-lua/plenary.nvim", lazy = true },
+    -- plenary - 其他插件使用的库
+    { "nvim-lua/plenary.nvim", lazy = true },
 }
