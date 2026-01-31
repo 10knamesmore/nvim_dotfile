c7ca2bd8fe5f54bd9c200956fbabc95b17949a88
wanger <310779211wym@gmail.com>
2026-02-01 00:32:21 +0800

    improve minifile

--- lua/plugins/editor/files/mini-files.lua
+++ lua/plugins/editor/files/mini-files.lua
@@ -69,11 +69,21 @@ return {
             require("mini.files").refresh({ content = { filter = new_filter } })
         end
 
+        local yank_path = function()
+            local path = (MiniFiles.get_fs_entry() or {}).path
+            if path == nil then
+                return vim.notify("Cursor is not on valid entry")
+            end
+            vim.notify("Yanked: " .. path)
+            vim.fn.setreg(vim.v.register, path)
+        end
+
         vim.api.nvim_create_autocmd("User", {
             pattern = "MiniFilesBufferCreate",
             callback = function(args)
                 local buf_id = args.data.buf_id
                 vim.keymap.set("n", ".", toggle_dotfiles, { buffer = buf_id, desc = "Toggle hidden files" })
+                vim.keymap.set("n", "gy", yank_path, { buffer = buf_id, desc = "Yank path" })
             end,
         })
 
