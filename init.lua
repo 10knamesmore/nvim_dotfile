e7eaf520d2c54a36d0d60bb0d8c7296ae1e7507f
wanger <310779211wym@gmail.com>
2026-02-08 22:37:01 +0800

    fix neovide

--- init.lua
+++ init.lua
@@ -6,6 +6,7 @@ if vim.g.neovide then
     require("config.lazy")
     require("config.keymaps")
     require("config.options")
+    require("config.autocmds")
     -- 字体
     vim.o.guifont = "Hack Nerd Font:h10"
 
@@ -21,28 +22,6 @@ if vim.g.neovide then
     vim.g.neovide_cursor_animation_length = 0.08
     vim.g.neovide_cursor_trail_size = 1
     vim.g.neovide_cursor_animate_in_insert_mode = true
-
--- vim.g.neovide_cursor_animation_length = 0
--- vim.g.neovide_cursor_trail_size = 0
--- vim.g.neovide_cursor_animate_in_insert_mode = false
-
--- 全屏
--- vim.g.neovide_fullscreen = true
---
-
--- 输入的时候不显示鼠标
--- vim.g.neovide_hide_mouse_when_typing = true
-
--- 记住窗口大小
--- vim.g.neovide_remember_window_size = true
-
--- 全屏 --
--- vim.keymap.set(
---     { "n" },
---     "<leader>wf",
---     ":let g:neovide_fullscreen = !g:neovide_fullscreen<CR>",
---     { noremap = true, silent = true, desc = "全屏" }
--- )
 elseif vim.g.vscode then
     require("code.config.options")
     require("code.config.keymaps")
